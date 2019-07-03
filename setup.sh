10ac6a4f746cdeb722bbfe5f8b19fdc9b4dbbf07
Elran Shefer <elran777@gmail.com>
2019-07-03 00:59:32 -0700

    added setup.sh for linux users

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
+set -e
+sudo apt-get update
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa  # repo for python3
 sudo apt-get update
 
@@ -8,14 +11,16 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # install python env
-sudo apt-get install -y python3.7 python3-pip python3-venv
-mkdir .venv
-python3 -m venv .venv/butler
-source .venv/butler/bin/activate
-sudo apt-get install -y build-essential libssl-dev libffi-dev python3.7-dev
+sudo apt-get install -y python3 python3-pip python3-venv virtualenvwrapper
+source "/home/$USER/.local/bin/virtualenvwrapper.sh"
+grep -qxF 'source "/home/$USER/.local/bin/virtualenvwrapper.sh"' ~/.bashrc || echo 'source "/home/$USER/.local/bin/virtualenvwrapper.sh"' >> ~/.bashrc
+source ~/.bashrc
+mkvirtualenv butler
+workon butler
+sudo apt-get install -y build-essential libssl-dev libffi-dev python3-dev
 
 # install pip requirements
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt -e .
 
 # install npm dependencies
 npm install
