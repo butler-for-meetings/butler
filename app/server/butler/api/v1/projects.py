814824e186b3c84116fba08d84718b0f0a803c48
theonlyartz <callcraft456@gmail.com>
2019-07-22 17:43:55 +0300

    Added initial-debug routes

--- app/server/butler/api/v1/projects.py
+++ app/server/butler/api/v1/projects.py
@@ -1,6 +1,14 @@
 from flask import Blueprint
+from butler.db.models import Project
 
 projects_blueprint = Blueprint('projects', __name__)
-@projects_blueprint.route("getProject")
-def get_project():
-    return "WOAH!"
+@projects_blueprint.route("get/<string:id>")
+def get_project(id):
+    project = Project.objects.filter(refs__in=[id])
+    print(project)
+    return ""
+
+@projects_blueprint.route("create", methods=["POST"])
+def create_project():
+    new = Project.objects.create(title="NIssg")
+    return "a"
