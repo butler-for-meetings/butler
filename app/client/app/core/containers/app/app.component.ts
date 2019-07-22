a507816c6fb74ce334fb3ecec314ef0ca8d5ca03
berale <weberale7@gmail.com>
2019-07-22 15:41:08 +0300

    Add mimic to project

--- app/client/app/core/containers/app/app.component.ts
+++ app/client/app/core/containers/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import mimic from 'mimic';
+import * as mocks from './mocks.json';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor() {
+    this.addMockData();
+  }
+  
+  addMockData() {
+    mimic.import(JSON.stringify(mocks.default));
+  }
 }
