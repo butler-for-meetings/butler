c2505e8e0368604d9ca57e66d30f0970e8b06642
Mariel <mariel.resnik@gmail.com>
2019-07-22 19:59:03 +0300

    Deleted issue class file

--- app/server/butler/api/jira/Issue.py
+++
@@ -1,9 +0,0 @@
-
-class Issue:
-    def __init__(self, project, summary, description=None, type='Task', status='To Do', assignee=None):
-        self.project = project
-        self.summary = summary
-        self.description = description
-        self.type = type
-        self.status = status
-        self.assignee = assignee
