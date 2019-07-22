f5a87d8ee224a7c8cecb823a27327b0e3c7c5e48
Liron Taub <taub12@gmail.com>
2019-07-22 17:13:18 +0300

    Basic layout structure fixes

--- app/client/app/core/containers/home/home.component.ts
+++ app/client/app/core/containers/home/home.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  //@ViewChild('drawer',{static: true}) drawer: MatDrawer;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeDrawerState(e) {
+    //this.drawer.toggle();
   }
 
 }
