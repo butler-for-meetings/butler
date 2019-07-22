3487787493063e5febf942ea4aa5137902a9b655
Mariel <mariel.resnik@gmail.com>
2019-07-22 20:21:27 +0300

    Deleted issue class file

--- app/server/butler/api/jira/Jira.py
+++ app/server/butler/api/jira/Jira.py
@@ -1,28 +1,50 @@
-import auth_keys
 from jira import JIRA
+import auth_keys
 
+"""
+Jira class, manages the connection to jira.
+"""
 
-class Jira:
+class jira:
+    """
+    jira class
+    """
     def __init__(self):
         self.jira_handler = JIRA('https://butlertest.atlassian.net/',
                                  basic_auth=(auth_keys.USER, auth_keys.TOKEN))
 
-    def add_issue(self, project, summary, description=None, type='Task'):
+    def add_issue(self, project, summary, description=None, issue_type='Task'):
+        """
+        :param project: project key
+        :param summary: issue summary
+        :param description: issue description
+        :param issue_type: issue type
+        """
         self.jira_handler.create_issue(project=project, summary=summary,
                                        description=description,
-                                       issuetype={'name': type})
+                                       issuetype={'name': issue_type})
 
     # only from project manager's user
     def delete_issue(self, key):
+        """
+        :param key:  issue key
+        """
         issue_to_delete = self.jira_handler.issue(key)
         issue_to_delete.delete()
 
     def update_issue(self, key, fields):
+        """
+        :param key: issue key
+        :param fields: dictionary of fields
+        """
         issue_to_update = self.jira_handler.issue(key)
         issue_to_update.update(fields)
 
     def get_project_issues(self, project_key):
-        # returns a dictionary <issue-key,issue-fields>
+        """
+        :param project_key: project key
+        :return: dictionary <issue-key,issue-fields>
+        """
         issues_in_proj = self.jira_handler.search_issues(
             'project={}'.format(project_key))
         lst_of_issues = {}
@@ -31,13 +53,24 @@ class Jira:
         return lst_of_issues
 
     def find_issue(self, key):
+        """
+        :param key: issue key
+        :return: issue
+        """
         return self.jira_handler.issue(key)
 
     def add_comment(self, key, comment):
+        """
+        :param key: issue key
+        :param comment: issue comment
+        """
         issue = self.find_issue(key)
         self.jira_handler.add_comment(issue, comment)
-        return
 
     def add_attachment(self, file_path, key):
+        """
+        :param file_path: file path of attachment
+        :param key: issue key
+        """
         issue = self.find_issue(key)
         self.jira_handler.add_attachment(issue=issue, attachment=file_path)
--- app/server/butler/api/jira/auth_keys.py
+++ app/server/butler/api/jira/auth_keys.py
@@ -1,2 +1,6 @@
+"""
+api credentials. user is the regular jira username. token is a token
+generated using this link:  https://id.atlassian.com/manage/api-tokens
+"""
 USER = ''
 TOKEN = ''
