2f3da9406e1dd90bc103cde47c509c8a2daf7420
asaf <asafsemail@gmail.com>
2019-07-22 14:24:19 +0300

    fix comment reference field

--- app/server/butler/db/models.py
+++ app/server/butler/db/models.py
@@ -1,11 +1,12 @@
-<<<<<<< HEAD
 from mongoengine import *
 import datetime
+
 
 class User(Document):
     email = EmailField(required=True)
     first_name = StringField(max_length=64, required=True)
     last_name = StringField(max_length=64, required=True)
+
 
 class Task(EmbeddedDocument):
     finished = BooleanField(default=False)
@@ -13,9 +14,11 @@ class Task(EmbeddedDocument):
     start_date = DateTimeField(default=datetime.datetime.utcnow, required=True)
     end_date = DateTimeField(required=True)
 
+
 class Comment(Document):
     content = StringField(required=True)
-    author = User(required=True)
+    author = ReferenceField(User, required=True)
+
 
 class Discussion(Document):
     title = StringField(required=True)
@@ -31,19 +34,15 @@ class Discussion(Document):
     main_points_sum = ListField(StringField)
     comments = ListField(Comment)
 
+
 class Project(Document):
- 		title = StringField(required=True)
+    title = StringField(required=True)
     participants = ListField(ReferenceField(User))
     start_date = DateTimeField(default=datetime.datetime.utcnow, required=True)
     end_date = DateTimeField(required=True)
     discussions = ListField(ReferenceField(Discussion))
 
+
 class Tag(Document):
     name = StringField(required=True)
-=======
 
-
-if __name__ == '__main__':
-    from mongoengine import connect
-    connect(host='mongodb+srv://admin:@butler-mqkbs.azure.mongodb.net/test?retryWrites=true&w=majority')
->>>>>>> d0d0fd7d57e7b853321cc16ed446b93f388c1285
