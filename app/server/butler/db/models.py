2e2faa4d50de5992db6bba123444c5b969faf854
theonlyartz <callcraft456@gmail.com>
2019-07-22 17:43:39 +0300

    required should be false for start since it has default

--- app/server/butler/db/models.py
+++ app/server/butler/db/models.py
@@ -11,14 +11,13 @@ class User(Document):
 class Task(EmbeddedDocument):
     finished = BooleanField(default=False)
     responsible = ReferenceField(User, required=True)
-    start_date = DateTimeField(default=datetime.datetime.utcnow, required=True)
-    end_date = DateTimeField(required=True)
+    start_date = DateTimeField(default=datetime.datetime.utcnow)
+    end_date = DateTimeField(default=datetime.datetime.utcnow, required=True)
 
 
 class Comment(Document):
     content = StringField(required=True)
     author = ReferenceField(User, required=True)
-
 
 class Discussion(Document):
     title = StringField(required=True)
@@ -38,11 +37,10 @@ class Discussion(Document):
 class Project(Document):
     title = StringField(required=True)
     participants = ListField(ReferenceField(User))
-    start_date = DateTimeField(default=datetime.datetime.utcnow, required=True)
+    start_date = DateTimeField(default=datetime.datetime.utcnow)
     end_date = DateTimeField(required=True)
     discussions = ListField(ReferenceField(Discussion))
 
 
 class Tag(Document):
     name = StringField(required=True)
-
