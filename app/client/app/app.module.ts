6d0c8fbf8cd8fc567d173565ef0471cd024dcce6
Liron Taub <taub12@gmail.com>
2019-07-22 13:35:40 +0300

    Added dependencies and basic theme

--- app/client/app/app.module.ts
+++ app/client/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './core/app/app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { HomeComponent } from './core/home/home.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +17,9 @@ import { HomeComponent } from './core/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [
