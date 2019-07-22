3e597186ea4272c3211b977a1260880a36c5ef5d
gguurr <gurkovacs@gmail.com>
2019-07-22 17:00:02 +0300

    fixed errors in utils.py

--- app/server/butler/api/confluence/utils.py
+++ app/server/butler/api/confluence/utils.py
@@ -1,54 +1,54 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 """"
-  Utils for the conf
+    Utils for the conf
 """
 env = Environment(
-    loader = FileSystemLoader('./templates'),
-    autoescape = select_autoescape(['html'])
+    loader=FileSystemLoader('./templates'),
+    autoescape=select_autoescape(['html'])
 )
 
 def render_to_confluence(discussion):
-  """
-  discussion: data form db
-  """
-  template = env.get_template('template01.html')
+    """
+    discussion: data form db
+    """
+    template = env.get_template('template01.html')
 
-  comments = []
-  for c in discussion.comments:
-    comments.append(
-      {
-        "author": {
-          "name": c.author.first_name + " " + c.author.first_name,
-          "link": "#"
-        },
-        "content": c.content
-      }
-    )
+    comments = []
+    for c in discussion.comments:
+        comments.append(
+            {
+                "author": {
+                  "name": c.author.first_name + " " + c.author.first_name,
+                  "link": "#"
+                },
+                "content": c.content
+            }
+        )
 
-  participants = []
-  for p in discussion.participants:
-    participants.append(
-      {
-        "name": p.first_name + " " + p.first_name,
-        "link": "#"
-      }
-    )
-  tasks = []
-  for t in discussion.tasks:
-    tasks.append(
-      {
-        "name": t.name,
-        "finished": t.finished,
-        "responsible": {
-          "name": t.responsible.first_name + " " + t.responsible.first_name,
-          "link": "#"
-        },
-        "start_date": t.start_date,
-        "end_date": t.end_date
-      }
-    )
-  data = {
+    participants = []
+    for p in discussion.participants:
+       participants.append(
+          {
+            "name": p.first_name + " " + p.first_name,
+            "link": "#"
+          }
+       )
+    tasks = []
+    for t in discussion.tasks:
+        tasks.append(
+            {
+            "name": t.name,
+            "finished": t.finished,
+            "responsible": {
+                "name": t.responsible.first_name + " " + t.responsible.first_name,
+                "link": "#"
+            },
+            "start_date": t.start_date,
+            "end_date": t.end_date
+            }
+        )
+    data = {
     'title': discussion.title,
     'previous_discussion': {
       'title': discussion.previous_discussion.title,
@@ -57,7 +57,7 @@ def render_to_confluence(discussion):
     'tasks': tasks,
     'date': discussion.date,
     "user": {
-      "name": discussion.host.first_name + " " + discussion.host.first_name,
+      "name": discussion.first_name + " " + discussion.first_name,
       "link": "#"
     },
     "participants": participants,
@@ -67,5 +67,5 @@ def render_to_confluence(discussion):
     "main_points": discussion.main_points,
     "main_points_sum": discussion.main_points_sum,
     "comments": comments
-  }
-  return template.render(data)
+    }
+    return template.render(data)
