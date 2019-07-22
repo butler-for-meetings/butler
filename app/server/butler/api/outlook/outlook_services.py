830f916d3564adc5dd333e6a8cce9f60f750b4b8
sagiv harel <sagivh203@gmail.com>
2019-07-22 16:37:40 +0300

    add outlook calenders access

---
+++ app/server/butler/api/outlook/outlook_services.py
@@ -0,0 +1,66 @@
+import requests
+import uuid
+import json
+
+graph_endpoint = 'https://graph.microsoft.com/v1.0{0}'
+
+# Generic API Sending
+def make_api_call(method, url, token, payload = None, parameters = None):
+  # Send these headers with all API calls
+  headers = { 'User-Agent' : 'python_tutorial/1.0',
+              'Authorization' : 'Bearer {0}'.format(token),
+              'Accept' : 'application/json' }
+
+  # Use these headers to instrument calls. Makes it easier
+  # to correlate requests and responses in case of problems
+  # and is a recommended best practice.
+  request_id = str(uuid.uuid4())
+  instrumentation = { 'client-request-id' : request_id,
+                      'return-client-request-id' : 'true' }
+
+  headers.update(instrumentation)
+
+  response = None
+
+  if (method.upper() == 'GET'):
+      response = requests.get(url, headers = headers, params = parameters)
+  elif (method.upper() == 'DELETE'):
+      response = requests.delete(url, headers = headers, params = parameters)
+  elif (method.upper() == 'PATCH'):
+      headers.update({ 'Content-Type' : 'application/json' })
+      response = requests.patch(url, headers = headers, data = json.dumps(payload), params = parameters)
+  elif (method.upper() == 'POST'):
+      headers.update({ 'Content-Type' : 'application/json' })
+      response = requests.post(url, headers = headers, data = json.dumps(payload), params = parameters)
+
+  return response
+
+
+
+def get_me(access_token):
+    get_me_url = graph_endpoint.format('/me')
+
+    # Use OData query parameters to control the results
+    #  - Only return the displayName and mail fields
+    query_parameters = {'$select': 'displayName,mail'}
+
+    results = make_api_call('GET', get_me_url, access_token, "", parameters = query_parameters)
+
+    if (results.status_code == 200):
+        return results.json()
+    else:
+        return "{0}: {1}".format(results.status_code, results.text)
+
+def get_events(access_token):
+    get_events_url = graph_endpoint.format('/me/events')
+
+    query_parameters = {
+        '$select': 'subject,body,bodyPreview,organizer,attendees,start,end,location'
+        }
+
+    results = make_api_call('GET', get_events_url, access_token, "", parameters = query_parameters)
+
+    if (results.status_code == 200):
+        return results.json()
+    else:
+        return "{0}: {1}".format(results.status_code, results.text)
--- app/server/butler/server.py
+++ app/server/butler/server.py
@@ -1,12 +1,15 @@
 """Butler server application - implements required api as a REST service."""
-from flask import Flask
-from gevent.pywsgi import WSGIServer
+from flask import Flask, request, session
 
+from gevent.pywsgi import WSGIServer
+from api.outlook.auth_helper import get_signin_url, get_token_from_code
+from api.outlook.outlook_services import *
 
 PORT = 5000
 
 
 APP = Flask(__name__)
+APP.secret_key = b'_5#y2L"F3Q8z\n\xec]/'
 
 
 @APP.route('/api/projects')
@@ -14,6 +17,35 @@ def index():
     """Example route for testings."""
     return "Hello World!"
 
+
+@APP.route('/api/outlook/getsigininurl')
+def get_outlook_sign_in_url():
+    REDIRECT_URI = 'http://localhost:5000/api/outlook/gettoken'
+    return get_signin_url(REDIRECT_URI)
+
+@APP.route('/api/outlook/gettoken')
+def get_token():
+    auth_code = request.args.get('code')
+    REDIRECT_URI = 'http://localhost:5000/api/outlook/gettoken'
+    token_res = get_token_from_code(auth_code, REDIRECT_URI)
+    access_token = token_res['access_token']
+    session['outlook_access_token'] = access_token
+
+    return "got token successfully! {}".format(access_token)
+    # return token_res['access_token']
+
+@APP.route('/api/outlook/getme') 
+def getme():
+    access_token = session['outlook_access_token']
+    user = get_me(access_token)
+    return "{}".format(user['displayName'])
+
+
+@APP.route('/api/outlook/getevents')
+def get_outlook_events():
+    access_token = session['outlook_access_token']
+    events = get_events(access_token)
+    return "{}".format(events)
 
 def production():
     """Start server application in production mode - wsgi server."""
@@ -24,6 +56,7 @@ def production():
 def develop():
     """Start server application in development mode - wsgi server."""
     APP.run(host='0.0.0.0', port=PORT, debug=True)
+
 
 
 if __name__ == '__main__':
