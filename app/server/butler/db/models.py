71fd5067820f29090aa9bac1ffadc9134f30ab9d
theonlyartz <callcraft456@gmail.com>
2019-07-22 14:21:59 +0300

    The base MongoDB models

---
+++ app/server/butler/db/models.py
@@ -0,0 +1,41 @@
+from mongoengine import *
+import datetime
+
+class User(Document):
+    email = EmailField(required=True)
+    first_name = StringField(max_length=64, required=True)
+    last_name = StringField(max_length=64, required=True)
+
+class Task(EmbeddedDocument):
+    finished = BooleanField(default=False)
+    responsible = ReferenceField(User, required=True)
+    start_date = DateTimeField(default=datetime.datetime.utcnow, required=True)
+    end_date = DateTimeField(required=True)
+
+class Comment(Document):
+    content = StringField(required=True)
+    author = User(required=True)
+
+class Discussion(Document):
+    title = StringField(required=True)
+    previous_discussion = LazyReferenceField(document_type="Discussion")
+    tasks = EmbeddedDocumentListField(Task)
+    date = DateTimeField(required=True)
+    host = ReferenceField(User, required=True)
+    participants = ListField(ReferenceField(User))
+    tags = ListField(StringField)
+    purpose = StringField(required=True)
+    background = StringField(required=True)
+    main_points = ListField(StringField)
+    main_points_sum = ListField(StringField)
+    comments = ListField(Comment)
+
+class Project(Document):
+ 		title = StringField(required=True)
+    participants = ListField(ReferenceField(User))
+    start_date = DateTimeField(default=datetime.datetime.utcnow, required=True)
+    end_date = DateTimeField(required=True)
+    discussions = ListField(ReferenceField(Discussion))
+
+class Tag(Document):
+    name = StringField(required=True)
