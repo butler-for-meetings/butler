236c7127783817581d21aac6eebb4732a2cbab02
callcraft456@gmail.com <callcraft456@gmail.com>
2019-07-27 14:48:43 +0300

    Moved get_events route and imported necessary modules

--- app/server/butler/api/components/outlook/routes.py
+++ app/server/butler/api/components/outlook/routes.py
@@ -1,3 +1,11 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, session
+from butler.api.components.outlook.outlook_services import get_events, get_me
+from butler.api.components.outlook.auth_helper import get_signin_url, get_token_from_code
 
 outlook_blueprint = Blueprint('outlook', __name__)
+
+@outlook_blueprint.route('get_events')
+def get_outlook_events():
+    access_token = session['outlook_access_token']
+    events = get_events(access_token)
+    return "{}".format(events)
