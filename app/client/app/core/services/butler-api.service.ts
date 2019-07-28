4e9867c590a6788fba72689dea937f3f45de6e18
callcraft456@gmail.com <callcraft456@gmail.com>
2019-07-28 14:34:22 +0300

    Added a method to get a specific project

--- app/client/app/core/services/butler-api.service.ts
+++ app/client/app/core/services/butler-api.service.ts
@@ -6,13 +6,25 @@ import { Project } from '../models/project';
   providedIn: 'root'
 })
 export class ButlerApiService {
-
-  constructor(private _http: HttpClient) { }
+  public constructAPIRoute: (endpoint: string) => string;
+  constructor(private _http: HttpClient) {
+    this.constructAPIRoute = (endpoint: string): string => {
+      return `http://localhost:5000/api/v1/${endpoint}`;
+    }
+  }
 
   /**
-   * Get all projetcs
+   * Get all projects
    */
-  getAllProjetcs(): Promise<Project[]> {
-    return this._http.get<Project[]>('/api/projects').toPromise();
+  getAllProjects(): Promise<Project[]> {
+    return this._http.get<Project[]>(this.constructAPIRoute("projects")).toPromise();
+  }
+
+  /**
+  * Get a specific project using it's ID
+  * @param {string} id The ID of the project
+  */
+  getProject(id: string) : Promise<Project> {
+    return this._http.get<Project>(this.constructAPIRoute(`projects/${id}`)).toPromise();
   }
 }
